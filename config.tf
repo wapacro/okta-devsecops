1f9f31fa21e4db8940cf50aed292cde4d47b04a2
Roman Ackermann <roman.ackermann@okta.com>
2023-07-14 01:18:56 +0200

    Added AWS EC2 config

--- config.tf
+++ config.tf
@@ -3,3 +3,21 @@ variable "aws_region" {
   description = "AWS Region"
   default     = "us-west-1"
 }
+
+variable "aws_linux_image_id" {
+  type        = string
+  description = "AMI ID for Linux-based EC2 instances"
+  default     = "ami-0f8e81a3da6e2510a" # Ubuntu 22.04 LTS
+}
+
+variable "aws_windows_image_id" {
+  type        = string
+  description = "AMI ID for Windows-based EC2 instances"
+  default     = "ami-0b0de58bd9519cc54" # Windows Server 2022
+}
+
+variable "aws_instance_type" {
+  type        = string
+  description = "Instance type for EC2 instances"
+  default     = "t2.micro"
+}
