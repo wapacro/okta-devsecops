ed4a455ef034ea2118a29ca5d6bb37c4089c4abf
Roman Ackermann <roman.ackermann@okta.com>
2023-12-03 01:02:27 +0100

    Changed default AMIs for eu-central-2 AWS region

--- config.tf
+++ config.tf
@@ -50,13 +50,13 @@ variable "aws_region" {
 variable "aws_linux_image_id" {
   type        = string
   description = "AMI ID for Linux-based EC2 instances"
-  default     = "ami-0f8e81a3da6e2510a" # Ubuntu 22.04 LTS
+  default     = "ami-07cf963e6321c9e6a" # Ubuntu 22.04 LTS
 }
 
 variable "aws_windows_image_id" {
   type        = string
   description = "AMI ID for Windows-based EC2 instances"
-  default     = "ami-0b0de58bd9519cc54" # Windows Server 2022
+  default     = "ami-03bbfe1c798814651" # Windows Server 2022
 }
 
 variable "aws_instance_type" {
